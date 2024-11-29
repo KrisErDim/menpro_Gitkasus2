3859950929dc131cde8874ccb48c451fb36e0f4d
KrisErDim <iamerickktd@gmail.com>
2024-11-29 09:57:44 +0700

    nambah paragraph

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-    
 
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" conten="width=device-width">
+        <title>Git Kassus 2</title>
+</head>
+<body>
+    <h1>HI</h1>
+
+</body>
